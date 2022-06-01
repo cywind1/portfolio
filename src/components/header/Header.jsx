bc9af5613169df18735d2b96780920b269604c3e
cywind1 <chingjusttest@gmail.com>
2022-06-01 16:42:42 +0200

    t2_header_info

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import "./header.css";
+import CTA from "./CTA"
+import selfie from '../../assets/1_profile.jpg'
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className = "container header_container">
+        <h1> Susie Chan</h1>
+        <h2 className="text-light"> Web Developer</h2> 
+        <CTA/>
+        <HeaderSocials/>
+        <div className='box'>
+          <img src={selfie} alt='profile pic'/>
+        </div>
+        <a href='#contact' className='scroll_down'>Scroll Down</a>
+      </div> 
+
+    </header>
   )
 }
 
