4642e0c4a04f894e974973f4ed720308be572b53
cywind1 <chingjusttest@gmail.com>
2022-06-02 18:12:04 +0200

    t3_exp_jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
 import "./experience.css";
+import {BsPatchCheckFill} from 'react-icons/bs';
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+    <h2>My Experience</h2>
+      <div className='container exp_container'>
+      <div className='exp_frontend'>
+        <h3> Frontend Development </h3>
+        <div className='exp_content'>
+          <article className='exp_details'>
+            <BsPatchCheckFill/>
+            <h4>HTML</h4>
+            <small className='text-light'> Advanced </small>
+          </article>
+          <article className='exp_details'>
+            <BsPatchCheckFill/>
+            <h4>CSS</h4>
+            <small className='text-light'> Intermediate </small>
+          </article>
+          <article className='exp_details'>
+            <BsPatchCheckFill/>
+            <h4>JAVASCRIPT</h4>
+            <small className='text-light'> Beginner </small>
+          </article>
+          
+          <article className='exp_details'>
+            <BsPatchCheckFill/>
+            <h4>REACT</h4>
+            <small className='text-light'> Beginner </small>
+          </article>
+        </div>
+      </div>
+      
+      <div className='exp_backend'>
+        <h3> Backend Development </h3>
+        <div className='exp_content'>
+          <article className='exp_details'>
+            <BsPatchCheckFill/>
+            <h4>MONGO DB</h4>
+            <small className='text-light'> Intermediate </small>
+          </article>
+          <article className='exp_details'>
+            <BsPatchCheckFill/>
+            <h4>NODE JS</h4>
+            <small className='text-light'> Beginner </small>
+          </article>
+          <article className='exp_details'>
+            <BsPatchCheckFill/>
+            <h4>MY SQL</h4>
+            <small className='text-light'> Beginner </small>
+          </article>
+        </div>
+      </div>
+
+      </div>
+    </section>
   )
 }
 
