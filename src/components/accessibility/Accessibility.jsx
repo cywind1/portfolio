3abb6ec005bdc410fabd338aaf811ad538909968
cywind1 <chingjusttest@gmail.com>
2022-06-05 15:20:33 +0200

    t4_acc_jsx

--- src/components/accessibility/Accessibility.jsx
+++ src/components/accessibility/Accessibility.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import "./accessibility.css";
+import {BsCheckCircle} from "react-icons/bs";
 
 const Accessibility = () => {
   return (
-        <section id='accessibility'>Accessibility</section>
+        <section id='accessibility'>
+        <h2>Accessibility</h2>
+
+        <div className="container acc-container">
+          <article className ='accessibility'>
+            <div className ='acc-head' >
+              <h3>Checklist</h3>
+            </div>
+            <ul className='acc-list'>
+              <li>
+                <BsCheckCircle className='acc-list-icon'/> 
+                <p> Headings </p>
+              </li>
+              <li>
+                <BsCheckCircle className='acc-list-icon'/> 
+                <p> Text </p>
+              </li>
+              <li>
+                <BsCheckCircle className='acc-list-icon'/> 
+                <p> Images </p>
+              </li>
+              <li>
+                <BsCheckCircle className='acc-list-icon'/> 
+                <p>  Forms </p>
+              </li>
+              <li>
+                <BsCheckCircle className='acc-list-icon'/> 
+                <p> Page Navigation </p>
+              </li>
+              <li>
+                <BsCheckCircle className='acc-list-icon'/> 
+                <p> Links </p>
+              </li>
+            </ul>
+          </article>
+        </div>
+        </section>
   )
 }
 
