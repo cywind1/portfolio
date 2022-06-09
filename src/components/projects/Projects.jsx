b442b43f4aa28bc3d2799a1416b3e337822d4549
cywind1 <chingjusttest@gmail.com>
2022-06-09 11:43:16 +0200

    t6_projects_css

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,40 +1,61 @@
-import React from 'react';
+import React from "react";
 import "./projects.css";
-import projectImg1 from '../../assets/5.1_projects.jpg';
-import projectImg2 from '../../assets/5.2_projects.jpg';
-import projectImg3 from '../../assets/5.3_projects.jpg';
+import projectImg1 from "../../assets/5.1_projects.jpg";
+import projectImg2 from "../../assets/5.2_projects.jpg";
+import projectImg3 from "../../assets/5.3_projects.jpg";
 
 const Projects = () => {
   return (
-    <section id='projects'>
-          <h2>Projects</h2> 
-           <ul className='container pro-container'>
-          <li className='pro-card'>
-        <div className='pro-item-image'> 
-        <img src={projectImg1} alt=''/>
-        </div>
-        <h3> Screen Design Project </h3>
-            <a href='https://th-koeln.github.io/mi-bachelor-screendesign-projekte/sd-2020/chan-neubert-selim/' target='_blank'> Github Link</a>
-          </li>
+    <section id="projects">
+      <h2>Projects</h2>
+      <ul className="container pro-container">
+        <li className="pro-card">
+          <div className="pro-item-image">
+            <img src={projectImg1} alt="" />
+          </div>
+          <h3> Screen Design Project </h3>
+          <div className="pro-item-cta">
+            <a
+              href="https://th-koeln.github.io/mi-bachelor-screendesign-projekte/sd-2020/chan-neubert-selim/"
+              target="_blank"
+            >
+              {" "}
+              Github Link
+            </a>
+          </div>
+        </li>
 
- <li className='pro-card'>
-        <div className='pro-item-image'> 
-        <img src={projectImg2} alt=''/>
-        </div>
-        <h3> Internet of Things Project </h3>
-            <a href='https://github.com/aselim21/easygreeneryweb/' target='_blank'> Github Link</a>
-          </li>
+        <li className="pro-card">
+          <div className="pro-item-image">
+            <img src={projectImg2} alt="" />
+          </div>
+          <h3> Internet of Things Project </h3>
+          <div className="pro-item-cta">
+            <a
+              href="https://github.com/aselim21/easygreeneryweb/"
+              target="_blank"
+            >
+              {" "}
+              Github Link
+            </a>
+          </div>
+        </li>
 
-<li className='pro-card'>
-         <div className='pro-item-image'> 
-        <img src={projectImg3} alt=''/>
-        </div>
-        <h3> Web Service Project </h3>
-            <a href='https://enki-bookstore.herokuapp.com/' target='_blank'> Heroku Link</a>
-          </li>
-    </ul>
+        <li className="pro-card">
+          <div className="pro-item-image">
+            <img src={projectImg3} alt="" />
+          </div>
+          <h3> Web Service Project </h3>
+          <div className="pro-item-cta">
+            <a href="https://enki-bookstore.herokuapp.com/" target="_blank">
+              {" "}
+              Heroku Link
+            </a>
+          </div>{" "}
+        </li>
+      </ul>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
