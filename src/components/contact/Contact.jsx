7b660105dbe452a4e92c36543588e04581bd57cd
cywind1 <chingjusttest@gmail.com>
2022-06-09 17:12:26 +0200

    t6_contact_html

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React from "react";
 import "./contact.css";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 
 const Contact = () => {
   return (
-    <section id='contact'>
+    <section id="contact">
       <h2>Contact</h2>
-    </section>
-  )
-}
 
-export default Contact
+      <div className="container contact_container">
+        <div className="contact_cards">
+          <div className="contact_card">
+            <AiOutlineMail />
+            <h3>Email</h3>
+            <a href="mailto:ching_ying.chan@smail.th-koeln.de" target="_blank">
+              ching_ying.chan@smail.th-koeln.de
+            </a>
+          </div>
+          <div className="contact_card">
+            <AiFillGithub />
+            <h3>Github</h3>
+            <a href="https://github.com/cywind1" target="_blank">
+              https://github.com/cywind1
+            </a>
+          </div>
+        </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            type="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          />
+          <button type="reset"> Reset</button>
+          <button type="submit"> Submit</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import {SiLinkedin} from 'react-icons/si';
-import {SiGithub} from 'react-icons/si';
+import React from "react";
+import { SiLinkedin } from "react-icons/si";
+import { SiGithub } from "react-icons/si";
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
-        <a href='https://www.linkedin.com/in/ching-chan-24b696148/' role='img' aria-label='Linkedin icon'><SiLinkedin/></a>
-        <a href='https://github.com/cywind1/' role='img' aria-label='Github icon'><SiGithub/></a>        
+    <div className="header_socials">
+      <a
+        href="https://www.linkedin.com/in/ching-chan-24b696148/"
+        role="img"
+        aria-label="Linkedin icon"
+      >
+        <SiLinkedin />
+      </a>
+      <a href="https://github.com/cywind1/" role="img" aria-label="Github icon">
+        <SiGithub />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
