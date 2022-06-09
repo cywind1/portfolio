c95d66d333496c95dc00f3b176dea2e06f53591c
cywind1 <chingjusttest@gmail.com>
2022-06-09 12:29:24 +0200

    t6_project_data_map

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,55 +4,47 @@ import projectImg1 from "../../assets/5.1_projects.jpg";
 import projectImg2 from "../../assets/5.2_projects.jpg";
 import projectImg3 from "../../assets/5.3_projects.jpg";
 
+const data = [
+  {
+    id: 1,
+    image: projectImg1,
+    title: "Screen Design Project",
+    demo: "https://th-koeln.github.io/mi-bachelor-screendesign-projekte/sd-2020/chan-neubert-selim/",
+  },
+  {
+    id: 2,
+    image: projectImg2,
+    title: "Internet of Things Project",
+    demo: "https://github.com/aselim21/easygreeneryweb/",
+  },
+  {
+    id: 3,
+    image: projectImg3,
+    title: "Web Service Project",
+    demo: "https://enki-bookstore.herokuapp.com/",
+  },
+];
+
 const Projects = () => {
   return (
     <section id="projects">
       <h2>Projects</h2>
       <ul className="container pro-container">
-        <li className="pro-card">
-          <div className="pro-item-image">
-            <img src={projectImg1} alt="" />
-          </div>
-          <h3> Screen Design Project </h3>
-          <div className="pro-item-cta">
-            <a
-              href="https://th-koeln.github.io/mi-bachelor-screendesign-projekte/sd-2020/chan-neubert-selim/"
-              target="_blank"
-            >
-              {" "}
-              Github Link
-            </a>
-          </div>
-        </li>
-
-        <li className="pro-card">
-          <div className="pro-item-image">
-            <img src={projectImg2} alt="" />
-          </div>
-          <h3> Internet of Things Project </h3>
-          <div className="pro-item-cta">
-            <a
-              href="https://github.com/aselim21/easygreeneryweb/"
-              target="_blank"
-            >
-              {" "}
-              Github Link
-            </a>
-          </div>
-        </li>
-
-        <li className="pro-card">
-          <div className="pro-item-image">
-            <img src={projectImg3} alt="" />
-          </div>
-          <h3> Web Service Project </h3>
-          <div className="pro-item-cta">
-            <a href="https://enki-bookstore.herokuapp.com/" target="_blank">
-              {" "}
-              Heroku Link
-            </a>
-          </div>{" "}
-        </li>
+        {data.map(({ id, image, title, demo }) => {
+          return (
+            <li key={id} className="pro-card">
+              <div className="pro-item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3> {title} </h3>
+              <div className="pro-item-cta">
+                <a href={demo} target="_blank">
+                  Project Link
+                </a>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
